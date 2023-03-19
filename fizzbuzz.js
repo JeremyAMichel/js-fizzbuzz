d2e4f193fd17d7418bb45373fe497affeb79ca0c
JeremyAMichel <jeremy.a.michel@hotmail.fr>
2023-03-19 13:01:30 +0100

    function fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -7,9 +7,25 @@
 'use strict';
 
 var fizzbuzz = function (x) {
-//
-// YOUR CODE GOES HERE
-//
+
+    let printable = "";
+
+    if (x % 3 === 0 && x % 5 !=0){
+        return printable += 'fizz';
+    }
+
+    else if (x % 5 === 0 && x % 3 != 0){
+        return printable += 'buzz';
+    }
+
+    else if (x % 3 === 0 && x % 5 === 0){
+        return printable += 'fizzbuzz';
+    }
+
+    else {
+        return x;
+    }
+
 };
 
 module.exports = { fizzbuzz: fizzbuzz };
